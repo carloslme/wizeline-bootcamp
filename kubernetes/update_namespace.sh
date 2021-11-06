bda74a9337927acfceed826149c1a39bd89169e8
carloslme <32799711+carloslme@users.noreply.github.com>
2021-11-06 13:05:35 -0600

    feat: add creation of namespace and echo comments

--- kubernetes/update_namespace.sh
+++ kubernetes/update_namespace.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
+echo INFO: changing directory
 cd ~/wizeline-bootcamp/kubernetes
-echo hello
+pwd
+echo INFO: deleting namespace
+kubectl delete namespace airflow
+echo INFO: creating namespace
+kubectl create namespace airflow
+echo INFO: adding apache-airflow
+helm repo add apache-airflow https://airflow.apache.org
+echo INFO: installing apache-airflow into the airflow namespace
+helm install airflow apache-airflow/airflow --namespace airflow
