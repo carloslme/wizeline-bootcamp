e81911b37dc9534ce8daeb73e00ed748783636ba
carloslme <32799711+carloslme@users.noreply.github.com>
2021-11-13 21:20:11 -0600

    feat: add query to create table

--- extract_load_postgres_dag.py
+++ extract_load_postgres_dag.py
@@ -6,25 +6,25 @@ from airflow.operators.postgres_operator import PostgresOperator
 
 
 dag_params = {
-    'dag_id': 'PostgresOperator_dag',
-    'start_date': datetime(2019, 10, 7),
-    'schedule_interval': None
+    "dag_id": "extract_load_dag",
+    "start_date": datetime(2019, 10, 7),
+    "schedule_interval": None,
 }
 
 with DAG(**dag_params) as dag:
 
     create_table = PostgresOperator(
-        task_id='create_table',
-        sql='''CREATE TABLE new_table(
-            custom_id integer NOT NULL, timestamp TIMESTAMP NOT NULL, user_id VARCHAR (50) NOT NULL
-            );''',
+        task_id="create_table",
+        sql="""CREATE TABLE user_purchase(
+                invoice_number VARCHAR(10) NOT NULL,
+                stock_code VARCHAR(20) NOT NULL,
+                detail VARCHAR(1000) NOT NULL,
+                quantity INTEGER NOT NULL,
+                invoice_date TIMESTAMP NOT NULL,
+                unit_price NUMERIC (8, 3) NOT NULL,
+                customer_id INTEGER NOT NULL,
+                country VARCHAR(20) NOT NULL
+            );""",
     )
 
-    insert_row = PostgresOperator(
-    task_id='insert_row',
-    sql='INSERT INTO new_table VALUES(%s, %s, %s)',
-    trigger_rule=TriggerRule.ALL_DONE,
-    parameters=(uuid.uuid4().int % 123456789, datetime.now(), uuid.uuid4().hex[:10])
-    )
-
-    create_table >> insert_row
+    create_table
