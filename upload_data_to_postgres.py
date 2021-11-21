ae8b20082377492595332a3b2442d501a881da98
carloslme <32799711+carloslme@users.noreply.github.com>
2021-11-20 20:35:09 -0600

    feat: add dag for uploading data to postgres

---
+++ upload_data_to_postgres.py
@@ -0,0 +1,63 @@
+import psycopg2
+from datetime import datetime
+from airflow import models
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.google.cloud.hooks.gcs import GCSHook
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+from airflow.utils.dates import days_ago
+
+
+def load_data_to_postgres():
+
+    # Download the file to a temporary directory and return a file handle
+    gcs_hook = GCSHook()
+    tmp_file = gcs_hook.provide_file(
+        object_url="gs://wizeline-bootcamp-330020/data_source/user_purchase - user_purchase.csv"
+    )
+
+    # Open Postgres Connection
+    get_postgres_conn = PostgresHook(postgres_conn_id="postgres_default").get_conn()
+    curr = get_postgres_conn.cursor()
+
+    # Load csv into postgres
+    with tmp_file as tmp:
+        tmp.flush()
+        with open(tmp.name) as fh:
+            curr.copy_expert(
+                "COPY bootcampdb.user_purchase FROM STDIN WITH CSV HEADER", fh
+            )
+            get_postgres_conn.commit()
+
+
+with models.DAG(
+    "upload-data-to-postgress",
+    schedule_interval="@once",  # Override to match your needs
+    start_date=days_ago(0),
+    tags=["load"],
+) as dag:
+
+    load_csv_to_table = PythonOperator(
+        task_id="load_data_to_postgres",
+        provide_context=True,
+        python_callable=load_data_to_postgres,
+    )
+
+    extract >> load
+
+with DAG(
+    dag_id="csv_on_gcs_to_postgres",
+    default_args=default_args,
+    schedule_interval="@once",
+    catchup=False,
+) as dag:
+
+    load_csv_to_table = PythonOperator(
+        task_id="load_csv_to_table",
+        provide_context=True,
+        python_callable=load_csv,
+    )
+
+    create_schema >> load_csv_to_table
