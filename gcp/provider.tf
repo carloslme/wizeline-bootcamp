3f4c06c4bec4ae45d756e71c3c8b3e54ac565aa8
carloslme <32799711+carloslme@users.noreply.github.com>
2021-11-24 21:32:49 -0600

    feat: add provider tf

--- gcp/provider.tf
+++ gcp/provider.tf
@@ -10,3 +10,9 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
+
+# Create a GCS Bucket
+resource "google_storage_bucket" "wizeline-bootcamp-330020" {
+name     = var.bucket_name
+location = var.region
+}
