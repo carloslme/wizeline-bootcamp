b60f816fd7b06d826bcd87ce4f15dc802bd9394b
carloslme <32799711+carloslme@users.noreply.github.com>
2021-11-27 11:24:15 -0600

    feat: change directory for creating one bucket

--- gcp/backend.tf
+++ gcp/backend.tf
@@ -1,6 +1,6 @@
 terraform {
 backend "gcs" {
-  bucket = "wizeline-bootcamp-330020"   # GCS bucket name to store terraform tfstate
-  prefix = "wizeline-bucket"            # Update to desired prefix name. Prefix name should be unique for each Terraform project having same remote state bucket.
+  bucket = "backend-330020"   # GCS bucket name to store terraform tfstate
+  prefix = "first-app"           # Update to desired prefix name. Prefix name should be unique for each Terraform project having same remote state bucket.
   }
 }
