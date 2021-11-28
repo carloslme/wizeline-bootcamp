28873594a67081172008225b5425ca914461db8d
carloslme <32799711+carloslme@users.noreply.github.com>
2021-11-28 10:06:29 -0600

    feat: add dag id

--- postgres_to_gcs.py
+++ postgres_to_gcs.py
@@ -10,19 +10,21 @@ from airflow.providers.google.cloud.transfers.postgres_to_gcs import (
 )
 from airflow.models import Variable
 
+
 PROJECT_ID = Variable.get("PROJECT_ID")
 GCS_BUCKET = Variable.get("STAGING_BUCKET")
 FILENAME = "test_file"
 SQL_QUERY = "select * from user_purchase;"
 
 with models.DAG(
-    dag_id="",
+    dag_id="postgres_to_gcs",
     schedule_interval="@once",  # Override to match your needs
     start_date=datetime(2021, 1, 1),
     catchup=False,
     tags=["postgres", "gcs"],
 ) as dag:
     upload_data = PostgresToGCSOperator(
+        postgres_conn_id="cloud_postgres_sql",
         task_id="get_data",
         sql=SQL_QUERY,
         bucket=GCS_BUCKET,
