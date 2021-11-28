140418c9e3b8276731002cd2f273002da98f7b8d
carloslme <32799711+carloslme@users.noreply.github.com>
2021-11-28 00:19:08 -0600

    feat: adapt code for accepting null values

--- schemas/user_behavior_metric.sql
+++ schemas/user_behavior_metric.sql
@@ -5,3 +5,11 @@ CREATE TABLE user_behavior_metric(
     review_count INTEGER,
     insert_date DATE
 );
+
+CREATE TABLE IF NOT EXISTS postgres.user_behavior_metric(
+    customer INTEGER NOT NULL,
+    amount_spent DECIMAL(18, 5) NOT NULL,
+    review_score INTEGER NOT NULL,
+    review_count INTEGER NOT NULL,
+    insert_date TIMESTAMP NOT NULL
+);
